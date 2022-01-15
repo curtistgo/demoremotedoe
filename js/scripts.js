a136ebb4a33692861f45c16fbfd9bb264b3586f5
Curtis <curtistgo@gmail.com>
2022-01-15 07:50:47 -0500

    I added javaScript to the social studies page and made the explanations more clear.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 const copyDate = new Date().getFullYear();
 document.querySelector("#the-date").innerHTML = "Copyright " + copyDate;
+
+// Generate the video and PDF resources
+
+const resource = `
+    <p>Video Lesson: <a href="https://www.youtube.com/watch?v=ffbE3Ucrt5g" target="_blank">The Transcontinental Railroad</a></p>
+    <p>PDF: <a  href="./history_document.pdf" target="_blank">assignment/homework</a></p>
+`;
+
+document.querySelector("#lesson_1").addEventListener("click", generateResources);
+function generateResources (event) {
+    event.preventDefault();
+    document.querySelector("#resources").innerHTML = resource;
+}
